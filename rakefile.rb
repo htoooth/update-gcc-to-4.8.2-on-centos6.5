e2ea0afd75f0adb2eeaf7de0213e5706bb0d4c79
Tao Huang <ht.anglenx@gmail.com>
2014-11-28 16:16:14 +0800

    update file

--- rakefile.rb
+++ rakefile.rb
@@ -1,22 +1,22 @@
-desc "install gcc 4.8.2 in centos6.5"
-task :install do
-    sh "wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo"
-    sh "yum -y devtoolset-2-toolchain"
+namespace :gcc do
+    desc "yum install gcc in centos6.5"
+    task :yum do
+        sh "wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo"
+        sh "yum -y devtoolset-2-toolchain"
+    end
+
+    desc "install from tarball such as gcc[4.9.0]"
+    task :compile ,[:version] do |t,argv|
+        sh 'wget http://ftp.gnu.org/gnu/gcc/gcc-#{argv.version}/gcc-#{argv.version}.tar.bz2'
+        sh 'tar -xf gcc-#{argv.version}.tar.gz'
+        cd "gcc-#{argv.version}.tar.gz}"
+        sh "./contrib/download_prerequisites"
+        makedirs "gcc_temp"
+        cd "gcc_temp"
+        sh "../configure –enable-checking=release –enable-languages=c,c++ –disable-multilib"
+
+        # if you have 4 core in cpu
+        sh "make -j4"
+        sh "make install"
+    end 
 end
-
-desc "install from tarball such as gcc[4.9.0]"
-task :gcc ,[:version] do |t,argv|
-    sh "wget http://ftp.gnu.org/gnu/gcc/gcc-#{argv[0]}/gcc-#{argv[0]}.tar.bz2"
-    sh "tar -xf gcc-#{argv[0].tar.gz}"
-    cd "gcc-#{argv[0].tar.gz}"
-    sh "./contrib/download_prerequisites"
-    makedirs "gcc_temp"
-    cd "gcc_temp"
-    sh "../configure –enable-checking=release –enable-languages=c,c++ –disable-multilib"
-
-    # if you have 4 core in cpu
-    sh "make -j4"
-    sh "make install"
-end
-
-task :default => :install
--- readme.md
+++ readme.md
@@ -3,4 +3,12 @@ Install gcc in centos
 
 centos:6.5 final
 
-Type `rake`
+Type `rake gcc:compile[4.9.1,your/path]`
+
+## FAQ
+
+### A:can't find "GLIBCXX_3.4.20"
+### Q: see this [link](http://blog.liudongkai.com/2014/05/12/GLIBCXX-not-found/), or `add /usr/local/lib64` in your `LD_LIBRARY_PATH` variable. 
+
+
+
